d9fde1a73bc630ca527057e7f9cf1e818fbd1567
zerozawa <zekaixie37@gmail.com>
2023-09-18 14:44:55 +0800

    feat: paircompletion conf updater & delete pair when existing

--- packages/extension/src/modules/pairCompletion.ts
+++ packages/extension/src/modules/pairCompletion.ts
@@ -2,17 +2,45 @@ import * as vscode from 'vscode'
 import { IPairCompletion } from '../common/types'
 import * as state from '../common/state'
 import * as config from '../config'
+import * as R from 'ramda'
 
 export const init = (context: vscode.ExtensionContext) => {
-  context.subscriptions.push(...onChangeDocument(config.get()))
+  context.subscriptions.push(...onChangeDocument(config.get()), onChangeConf(context))
 }
 
-const onChangeDocument = ({ pairCompletionChars }: IPairCompletion) => {
+const onChangeConf = (context: vscode.ExtensionContext) =>
+  vscode.workspace.onDidChangeConfiguration(async (event) => {
+    if (
+      !event.affectsConfiguration(`${state.extPrefix}.${R.identity<keyof IPairCompletion>('pairCompletion')}`) &&
+      !event.affectsConfiguration(`${state.extPrefix}.${R.identity<keyof IPairCompletion>('pairCompletionChars')}`)
+    )
+      return
+    context.subscriptions.push(...onChangeDocument(config.get()))
+  })
+
+const onChangeDocument = (() => {
+  let pairConf: IPairCompletion | undefined = undefined
+  let hooks: vscode.Disposable[] = []
+  return (conf: IPairCompletion) => {
+    if (
+      conf.pairCompletion !== pairConf?.pairCompletion ||
+      conf.pairCompletionChars !== pairConf?.pairCompletionChars
+    ) {
+      pairConf = conf
+      hooks.map((hook) => hook.dispose())
+      hooks = makeHooks(conf)
+    }
+    return hooks
+  }
+})()
+
+const makeHooks = ({ pairCompletionChars, pairCompletion }: IPairCompletion) => {
   const charsGroups = pairCompletionChars.map((chars) => chars.split('')).filter((chars) => chars.length === 2)
   let selected: { range: vscode.Range; text: string }[] = []
   let isPairCompleting = false
   return [
     vscode.window.onDidChangeTextEditorSelection((event) => {
+      if (!pairCompletion) return
       isPairCompleting = false
       selected = []
       if (!state.funcTarget.pairCompletion.includes(event.textEditor.document.languageId)) return
@@ -24,6 +52,7 @@ const onChangeDocument = ({ pairCompletionChars }: IPairCompletion) => {
       })
     }),
     vscode.workspace.onDidChangeTextDocument((event) => {
+      if (!pairCompletion) return
       if (event.reason) return
       const editor = vscode.window.activeTextEditor
       if (!editor) return
@@ -33,22 +62,33 @@ const onChangeDocument = ({ pairCompletionChars }: IPairCompletion) => {
       }
       if (!state.funcTarget.pairCompletion.includes(event.document.languageId)) return
       if (event.contentChanges[0].text.length !== 1) return
-      const chars = charsGroups.find((chPair) => chPair.includes(event.contentChanges[0].text))
+      const chars = charsGroups?.find((chPair) => chPair.includes(event.contentChanges[0].text))
       if (!chars) return
       isPairCompleting = true
-      editor.edit((editBuilder) => {
-        selected.forEach(({ range, text }) => {
-          editBuilder.delete(new vscode.Range(range.start, range.start.translate(0, 1)))
-          if (text.length !== 0) {
-            editBuilder.insert(range.start, `${chars[0]}${text}${chars[1]}`)
-          } else {
-            editBuilder.insert(range.start, `${chars[0]}${chars[1]}`)
-          }
-        })
-      })
-      editor.selections = selected.map(
-        ({ range }) => new vscode.Selection(range.start.translate(0, 1), range.end.translate(0, 1)),
-      )
+      replacer(chars, editor, event.document, selected)
     }),
   ]
 }
+
+const replacer = (
+  chars: string[],
+  editor: vscode.TextEditor,
+  document: vscode.TextDocument,
+  selected: { range: vscode.Range; text: string }[],
+) =>
+  (editor.selections = selected.map(({ range, text }) => {
+    const nowRangeWithStartEnd = new vscode.Range(range.start.translate(0, -1), range.start.translate(0, 2))
+    const nowTextWithStartEnd = document.getText(nowRangeWithStartEnd)
+    if (nowTextWithStartEnd.startsWith(chars[0]) && nowTextWithStartEnd.endsWith(chars[1])) {
+      editor.edit((editBuilder) => editBuilder.replace(nowRangeWithStartEnd, text))
+      return new vscode.Selection(range.start.translate(0, -1), range.end.translate(0, -1))
+    } else {
+      editor.edit((editBuilder) =>
+        editBuilder.replace(
+          new vscode.Range(range.start, range.start.translate(0, 1)),
+          `${chars[0]}${text}${chars[1]}`,
+        ),
+      )
+      return new vscode.Selection(range.start.translate(0, 1), range.end.translate(0, 1))
+    }
+  }))
